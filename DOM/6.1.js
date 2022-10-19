f13ea6992885c7b48cd24f2b033fa7895f23f7c2
YasinCoban123 <9018681@student.zadkine.nl>
2022-10-19 10:01:00 +0200

    YC

---
+++ DOM/6.1.js
@@ -0,0 +1,16 @@
+alert("Je madre");
+// kleur aanpassen
+//document.getElementById("title").style.color = "red";
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 + getal2;
+alert("het antwoord is: " + som);
+
+// promptbox
+var kleur = prompt("geef een engelse kleurnaam: (red or blue", "");
+
+document.getElementById("title").style.color = kleur ;
+// tekst aanpassen
+document.getElementById("tekst1").innerHTML = "Inhoud aangepast.";
+}
